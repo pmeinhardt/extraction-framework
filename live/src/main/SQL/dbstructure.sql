ba3f454117930148228f9aac5d53ac3df064e619
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2014-05-06 10:07:05 +0200

    Create database trigger to track resource changes
    Creates a new row in the DBPEDIALIVE_UPDATES table for each new
    extraction in the DBPEDIALIVE_CACHE, recording its `pageID` and
    `updated` time. Needs testing.

--- live/src/main/SQL/dbstructure.sql
+++ live/src/main/SQL/dbstructure.sql
@@ -20,6 +20,35 @@ CREATE TABLE IF NOT EXISTS `DBPEDIALIVE_CACHE` (
   KEY `updated_index` (`updated`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ENGINE = InnoDB ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=4;
 
+--
+-- Create the update tracking table `DBPEDIALIVE_UPDATES`
+--
+
+DROP TABLE IF EXISTS `DBPEDIALIVE_UPDATES`;
+CREATE TABLE IF NOT EXISTS `DBPEDIALIVE_UPDATES` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'The unique id for the update',
+  `pageID` int(11) NOT NULL DEFAULT '0' COMMENT 'The wikipedia page ID',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp of when the page was updated',
+
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ENGINE = InnoDB ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=4;
+
+-- Add an index on the DBPEDIALIVE_UPDATES `pageID`
+
+CREATE INDEX `updates_page_id_index` ON `DBPEDIALIVE_UPDATES` (`pageID`);
+
+-- Track changes using a database trigger on the cache table
+
+DELIMITER |
+
+CREATE TRIGGER trackchanges AFTER UPDATE ON `DBPEDIALIVE_CACHE`
+FOR EACH ROW BEGIN
+  INSERT INTO `DBPEDIALIVE_UPDATES` (pageID, timestamp) VALUES (NEW.pageID, NEW.updated);
+END;
+|
+
+DELIMITER ;
+
 -- We use innodb_file_per_table=1; innodb_file_format=Barracuda; ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=4
 -- because in English Wikipedia the cache can reach up to 200GB!!!
 -- This way we reduce I/O and space a lot. It makes the db a little slower but it is also easier to recover
