1e5643f368fb78cbf4017b2085ff48b942dc0bc9
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2014-05-18 01:12:42 +0200

    Set up Memento module with Scalatra webapp

---
+++ memento/src/main/scala/DevelopmentServer.scala
@@ -0,0 +1,23 @@
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
+
+object DevelopmentServer {
+  def main(args: Array[String]) {
+    val server = new Server(8081)
+    val context = new WebAppContext("src/main/webapp", "/")
+
+    context.setServer(server)
+    server.setHandler(context)
+
+    try {
+      server.start()
+      server.join()
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
+        System.exit(1)
+      }
+    }
+  }
+}
---
+++ memento/src/main/scala/org/dbpedia/extraction/memento/MementoServlet.scala
@@ -0,0 +1,9 @@
+package org.dbpedia.extraction.memento
+
+import org.scalatra._
+
+class MementoServlet extends ScalatraServlet {
+  get("/") {
+    "Hello World!"
+  }
+}
